623ca77ded917f7864dd6fcefb5d102b32083af2
Andres <jorge-620@hotmail.com>
2022-08-17 23:48:05 -0500

    Index

--- main.py
+++ main.py
@@ -18,18 +18,18 @@ app.add_middleware(
 )
 
 
-
+'''
 @app.get("/")
 def read_root():
     url = 'https://62f6640ba3bce3eed7c04b72.mockapi.io/items'
     response = requests.get(url, {}, timeout=5)
     return {"items": response.json() }
-
 '''
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-'''
+
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
