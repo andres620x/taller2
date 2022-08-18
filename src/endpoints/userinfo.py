a23af34e67e1e21ac2bb1a06ec7ed7dd45259d04
Andres <jorge-620@hotmail.com>
2022-08-17 20:41:46 -0500

    Ajuste

--- src/endpoints/userinfo.py
+++ src/endpoints/userinfo.py
@@ -23,6 +23,8 @@ async def read_root():
 async def read_user(idUsuario: str):
     url = 'https://62fd8a326e617f88deab3075.mockapi.io/infoUsers/infoUsers/'+idUsuario
     response = requests.get(url, {}, timeout=5)
+    if(response.status_code==  500):
+     raise HTTPException(status_code=204, detail="Item not found")
     jsonresponse = json.loads(response.text)
     return jsonresponse
 
@@ -32,6 +34,8 @@ async def add_user(user: UserInfoModel):
     url = 'https://62fd8a326e617f88deab3075.mockapi.io/infoUsers/infoUsers/'
     params = {'UserInfoModel':user}
     response = requests.post(url, params = params, timeout=5)
+    if(response.status_code==  404):
+     raise HTTPException(status_code=204, detail="Item not found")
     jsonresponse = json.loads(response.text)
     return jsonresponse
 
@@ -41,6 +45,8 @@ async def read_user(user: UserInfoModel):
     url = 'https://62fd8a326e617f88deab3075.mockapi.io/infoUsers/infoUsers/'
     params = {'UserInfoModel':user}
     response = requests.put(url, params = params, timeout=5)
+    if(response.status_code==  404):
+     raise HTTPException(status_code=204, detail="Item not found")
     jsonresponse = json.loads(response.text)
     return jsonresponse
 
@@ -51,6 +57,5 @@ async def read_user(idUsuario: str):
     response = requests.delete(url)
     if(response.status_code==  404):
      raise HTTPException(status_code=204, detail="Item not found")
-
     jsonresponse = json.loads(response.text)
     return jsonresponse
