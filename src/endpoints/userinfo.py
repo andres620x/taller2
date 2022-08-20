630627a20db0b11a5f070c89165a7b04a91c1e1f
Jorge Andres <jorge-620@hotmail.com>
2022-08-20 08:43:16 -0500

    Ajuste nombre variable

--- src/endpoints/userinfo.py
+++ src/endpoints/userinfo.py
@@ -27,9 +27,9 @@ async def read_root():
     logger.info("Hola logger usuarios")
     return {"usuarios": response.json() }
 
-@router.get("/{idUsuario}")
-async def read_user(idUsuario: str):
-    url = 'https://62fd8a326e617f88deab3075.mockapi.io/infoUsers/infoUsers/'+idUsuario
+@router.get("/{internalId}")
+async def read_user(internalId: str):
+    url = 'https://62fd8a326e617f88deab3075.mockapi.io/infoUsers/infoUsers/'+internalId
     response = requests.get(url, {}, timeout=5)
     if(response.status_code==  500):
      raise HTTPException(status_code=204, detail="Item not found")
@@ -46,9 +46,9 @@ async def add_user(user: UserInfoModel):
     return jsonresponse
 
 
-@router.put("/update/{idUsuario}")
-async def read_user(user: UserInfoModel,idUsuario: str):
-    url = 'https://62fd8a326e617f88deab3075.mockapi.io/infoUsers/infoUsers/'+idUsuario
+@router.put("/update/{internalId}")
+async def read_user(user: UserInfoModel,internalId: str):
+    url = 'https://62fd8a326e617f88deab3075.mockapi.io/infoUsers/infoUsers/'+internalId
     data=json.loads(user.json())
     response = requests.put(url, data = data, timeout=5)
     if(response.status_code==  404):
@@ -57,9 +57,9 @@ async def read_user(user: UserInfoModel,idUsuario: str):
     return jsonresponse
 
 
-@router.delete("/delete/{idUsuario}")
-async def read_user(idUsuario: str):
-    url = 'https://62fd8a326e617f88deab3075.mockapi.io/infoUsers/infoUsers/'+idUsuario
+@router.delete("/delete/{internalId}")
+async def read_user(internalId: str):
+    url = 'https://62fd8a326e617f88deab3075.mockapi.io/infoUsers/infoUsers/'+internalId
     response = requests.delete(url)
     if(response.status_code==  404):
      raise HTTPException(status_code=204, detail="Item not found")
